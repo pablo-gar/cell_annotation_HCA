ec0c465cee2428fbeaa72134ed965a585b666049
Ubuntu <ubuntu@ip-172-31-13-214.us-west-1.compute.internal>
2019-08-28 17:43:56 +0000

    Correct log1p and variable genes

--- pancreas/scripts/run_models_4_concatenatio.sh
+++ pancreas/scripts/run_models_4_concatenatio.sh
@@ -8,16 +8,16 @@ script_path="../../scripts/hca/loom/python"
 train_loom="paper_alternative.loom paper_alternative_GSE86469.loom paper_alternative_GSE84133.loom"
 test_loom="hca_cellTypes.loom"
 
-out_h5ad_train="paper_model_alternative_integrated.h5ad"
-out_h5ad_test="hca_model_alternative_integrated.h5ad"
+out_h5ad_train="paper_model_alternative_concatenated.h5ad"
+out_h5ad_test="hca_model_concatenated.h5ad"
 
-python3 $script_path/model_create_from_multiple.py svm_SVC cellType combat False True $out_h5ad_train $out_h5ad_test ../results/integrated/plots_integration/ $test_loom $train_loom
+python3 $script_path/model_create_from_multiple.py svm_SVC cellType concatenate True True $out_h5ad_train $out_h5ad_test ../results/integrated/plots_integration/ $test_loom $train_loom
 
 train_loom=${out_h5ad_train}
 test_loom=${out_h5ad_test}
 for model_name in RandomForest LogisticRegression MLPClassifier KNeighbors
 do
-    python3 $script_path/model_create_from_multiple.py $model_name cellType concatenate False False $out_h5ad_train $out_h5ad_test ../results/integrated/plots_integration/ $test_loom $train_loom 
+    python3 $script_path/model_create_from_multiple.py $model_name cellType concatenate True False $out_h5ad_train $out_h5ad_test ../results/integrated/plots_integration/ $test_loom $train_loom 
 done
 
 # Get benchmarking results
